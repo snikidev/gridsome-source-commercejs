3ebe9f8463279526f7fd2c69499abaa5cac5026e
Nikita K <n.kakuev@gmail.com>
2020-10-03 23:56:01 +0100

    Initial draft

---
+++ README.md
@@ -0,0 +1,28 @@
+# WIP: gridsome-source-commercejs
+
+Commerce.js source plugin for Gridsome
+
+## Install
+
+`yarn add gridsome-source-commercejs`
+
+or 
+
+`npm install gridsome-source-commercejs`
+
+## Usage
+
+In `gridsome.config.js` use in plugins
+
+```js
+export default {
+  plugins: [
+    {
+      use: 'gridsome-source-commercejs',
+      options: {
+        publicKey: 'pk_184625ed86f36703d7d233bcf6d519a4f9398f20048ec',
+      }
+    }
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const Commerce = require('@chec/commerce.js')
+
+const CommercejsPlugin = async (api, options) => {
+  if (!options.publicKey)
+    throw new Error('gridsome-source-commercejs: Missing Public Key')
+
+  const commerce = new Commerce(options.publicKey)
+
+  api
+    .loadSource(async actions => {
+      console.log('🛒 Fetching products from Commerce.js...')
+      
+      const { data: products } = await commerce.products.list()
+      const { data: categories } = await commerce.categories.list()
+
+      const productCollection = actions.addCollection({
+        typeName: 'CommercejsProducts',
+      })
+      const categoriesCollection = actions.addCollection({
+        typeName: 'CommercejsCategories',
+      })
+
+      for (const item of products) {
+        productCollection.addNode(item)
+      }
+
+      for (const item of categories) {
+        categoriesCollection.addNode(item)
+      }
+
+      console.log('✔️ Done fetching products from Commerce.js!')
+    })
+}
+
+module.exports = CommercejsPlugin
